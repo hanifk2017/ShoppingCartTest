b37d4da48e41f9df04d48bb7604aa1e198479649
hanifk2017 <hanifk@yahoo.com>
2018-06-14 11:52:16 +0100

    Changed to one liner

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -11,20 +11,33 @@ class ShoppingCart {
   def computeTotalWithDiscounts(cartItems: Seq[String]): BigDecimal = {
 
     // could possibly be simplified to a one liner with filter, zipWithIndex and modulus
+//
+//    // apple offer - buy 1 get 1 free
+//
+//    val noOfApples: Int = cartItems.map(_.toLowerCase).count(_ == "apple")
+//
+//    val effectiveNoOfApples: Int = noOfApples - noOfApples / 2
+//
+//    // orange offer - buy 3 for the price of 2
+//
+//    val noOfOranges: Int = cartItems.map(_.toLowerCase).count(_ == "orange")
+//
+//    val effectiveNoOfOranges: Int = noOfOranges - noOfOranges / 3
+//
+//    effectiveNoOfApples * stockItems("apple") + effectiveNoOfOranges * stockItems("orange")
 
-    // apple offer - buy 1 get 1 free
 
-    val noOfApples: Int = cartItems.map(_.toLowerCase).count(_ == "apple")
-
-    val effectiveNoOfApples: Int = noOfApples - noOfApples / 2
-
-    // orange offer - buy 3 for the price of 2
-
-    val noOfOranges: Int = cartItems.map(_.toLowerCase).count(_ == "orange")
-
-    val effectiveNoOfOranges: Int = noOfOranges - noOfOranges / 3
-
-    effectiveNoOfApples * stockItems("apple") + effectiveNoOfOranges * stockItems("orange")
+      cartItems
+        .map(_.toLowerCase)
+        .filter(x => x == "apple" || x == "orange")
+        .groupBy(x => x)
+        .mapValues(_.size)
+        .map {
+              case (k,v) if k == "apple"  => (k, (v  - (v/2)) * stockItems(k))
+              case (k,v) if k == "orange" => (k, (v  - (v/3)) * stockItems(k))
+          }
+        .values
+        .sum
 
   }
 
